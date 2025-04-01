9aa6b5c9333d52a4aa315da4d87595e39753ee46
Shreyansh99 <shreyanshkumarbasak@gmail.com>
2025-04-01 18:16:42 +0530

    about error fix

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 // app/page.js
 import Hero from "@/components/Hero";
-import  WhyUs from "@/components/about";
+// import  WhyUs from "@/components/about";
 import Contact from "@/components/contact";
 import Dept from "@/components/departmentSec";
 import HowItWorks from "@/components/howItWork";
