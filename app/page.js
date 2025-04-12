3419ba3d0bddea419a6fff0ee8bcb1beda964b91
Anshika Gupta <anshikagupta45612@gmail.com>
2025-04-12 23:37:56 +0530

    contact updated

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 // app/page.js
 import Hero from "@/components/Hero";
 import  WhyUs from "@/components/about";
-import Contact from "@/components/contact";
+import Contact from "./contact/page";
 import Dept from "@/components/departmentSec";
 import HowItWorks from "@/components/howItWork";
 
